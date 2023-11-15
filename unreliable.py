31eb437a1902633911d665fa80d3e5d7630c94e7
Sophie Zhao <59858562+mylightgarden@users.noreply.github.com>
2023-11-15 10:36:40 -0800

    Update unreliable.py

--- unreliable.py
+++ unreliable.py
@@ -1,19 +1,6 @@
 import random
-
-
-# #################################################################################################################### #
-# UnreliableChannel                                                                                                    #
-#                                                                                                                      #
-# Description:                                                                                                         #
-# This class is meant to be more of a blackbox but you are allowed to see the implementation. You are not allowed to   #
-# change anything in this file. There is also no need to base your algorithms on this particular implementation.       #
-#                                                                                                                      #
-#                                                                                                                      #
-# Notes:                                                                                                               #
-# This file is not to be changed.                                                                                      #
-#                                                                                                                      #
-#                                                                                                                      #
-# #################################################################################################################### #
+# UnreliableChannel                                                                                              
+# change anything in this file. There is also no need to base your algorithms on this particular implementation.       
 
 
 class UnreliableChannel():
